2e910258139373ba70ba02730bd2b6d29b44f527
Charlie Whittleman <brennwh@siue.edu>
2026-02-13 16:12:17 -0600

    Nix pls shutup abt dirty git tree

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,5 +1,8 @@
 # modules/common/nix.nix
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    warn-dirty = false;
+  };
 }
