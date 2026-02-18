d05eec086ffcc196b6cee68b90e67b5d2a13800b
Charlie Whittleman <brennwh@siue.edu>
2026-02-18 08:29:41 -0600

    maybe fixed kmonad

--- home/modules/kmonad.nix
+++ home/modules/kmonad.nix
@@ -5,20 +5,10 @@
 {
   home.packages = [ pkgs.kmonad ];
 
-  systemd.user.services.kmonad = {
-    Unit = {
-      Description = "KMonad keyboard remapping";
-      After = [ "graphical-session.target" ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.kmonad}/bin/kmonad ${Dotfiles}/kmonad/.config/kmonad/colemakHomerowMods.kbd";
-      Restart = "always";
-    };
-
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
+  services.kmonad = {
+    enable = true;
+    extraArgs = "${Dotfiles}/kmonad/.config/kmonad/colemakHomerowMods.kbd";
   };
+  
 }
 
