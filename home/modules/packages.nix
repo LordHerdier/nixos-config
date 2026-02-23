78533779d2c547390e12da8602d7a9fe916d9758
Charlie Whittleman <brennwh@siue.edu>
2026-02-23 13:34:16 -0600

    Add fastfetch

--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -1,46 +1,54 @@
 # home/modules/packages.nx
 
-{ pkgs, lib, isWsl, ... }:
+{
+  pkgs,
+  lib,
+  isWsl,
+  ...
+}:
 
 {
-  home.packages = (with pkgs; [
-    ansible
-    stylua
-    shfmt
-    shellcheck
-    neovim
-    oh-my-posh
-    pass
-    binwalk
-    unzip
-    sshpass
-    stow
-    zoxide
-    tmux
-    fzf
-    ripgrep
-    gnugrep
-    gnused
-    gh
-    gnupg
-    gcc
-    coreutils
-    procps
-    yazi
-    ffmpegthumbnailer
-    poppler-utils
-    unzip
-    jq
-    python3
-    qemu
-    wl-clipboard
-    nerd-fonts.caskaydia-cove
-    nmap
-   ]);
-#  ++ lib.optionals isWsl (with pkgs; [
-#    wslu
-#    win32yank
-#  ])
-#  ++ lib.optionals (!isWsl) [ pkgs.wl-clipboard ];
+  home.packages = (
+    with pkgs;
+    [
+      ansible
+      stylua
+      shfmt
+      shellcheck
+      neovim
+      oh-my-posh
+      pass
+      binwalk
+      unzip
+      sshpass
+      stow
+      zoxide
+      tmux
+      fzf
+      ripgrep
+      gnugrep
+      gnused
+      gh
+      gnupg
+      gcc
+      coreutils
+      procps
+      yazi
+      ffmpegthumbnailer
+      poppler-utils
+      unzip
+      jq
+      python3
+      qemu
+      fastfetch
+      wl-clipboard
+      nerd-fonts.caskaydia-cove
+      nmap
+    ]
+  );
+  #  ++ lib.optionals isWsl (with pkgs; [
+  #    wslu
+  #    win32yank
+  #  ])
+  #  ++ lib.optionals (!isWsl) [ pkgs.wl-clipboard ];
 }
-
--- home/modules/tmux/10-keybinds.nix
+++ home/modules/tmux/10-keybinds.nix
@@ -12,6 +12,11 @@
     bind-key k select-pane -U
     bind-key l select-pane -R
 
+    bind-key C-n select-pane -L
+    bind-key C-e select-pane -D 
+    bind-key C-u select-pane -U
+    bind-key C-i select-pane -R
+
     # Window navigation
     bind-key Tab next-window
     bind-key BTab previous-window
@@ -20,6 +25,12 @@
     bind c new-window -c "#{pane_current_path}"
     bind - split-window -v -c "#{pane_current_path}"
     bind _ split-window -h -c "#{pane_current_path}"
+    bind \ split-window -h -c "#{pane_current_path}"
+
+
+    # Session Switching
+    bind-key ] switch-client -n
+    bind-key [ switch-client -p
 
     # Copy mode (vi)
     bind-key -T copy-mode-vi 'v' send-keys -X begin-selection
