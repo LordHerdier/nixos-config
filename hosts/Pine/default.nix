3cd053cee953a0854b7b37b3c6f41f7433f75159
charlotte whittleman <charlotte@whittleman.net>
2026-02-23 20:16:30 -0600

    move steam to system modules

--- hosts/Pine/default.nix
+++ hosts/Pine/default.nix
@@ -14,6 +14,7 @@
     ../../modules/profiles/hyprland.nix
     ../../modules/common/tailscale.nix
     ../../modules/common/kmonad/kmonad.nix
+    ../../modules/common/steam.nix
   ];
 
   system.stateVersion = "25.11";
