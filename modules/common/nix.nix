5f6a0bfc510865db412fd41c6c00b8292198f899
Charlie Whittleman <brennwh@siue.edu>
2026-02-25 13:20:10 -0600

    Add optimizations for nix store/gc

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -2,11 +2,28 @@
 
 {
   nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
     warn-dirty = false;
   };
   nixpkgs.config = {
     allowUnfree = true;
+  };
+
+  nix.optimise.automatic = true;
+  nix.optimise.dates = [ "03:45" ];
+  nix.settings.auto-optimise-store = true;
+
+  nic.gc = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 0;
+      Minute = 0;
+    };
+    options = "--delete-older-than 7d";
   };
 
 }
