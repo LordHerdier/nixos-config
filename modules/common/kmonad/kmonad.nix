6268a77e36e360ed68437b3f824c244b52ff885f
charlotte whittleman <charlotte@whittleman.net>
2026-03-24 14:31:48 -0500

    Update dotfiles; allow charlotte to start/stop kmonad without sudo

--- modules/common/kmonad/kmonad.nix
+++ modules/common/kmonad/kmonad.nix
@@ -1,18 +1,28 @@
 # modules/common/kmonad/default.nix
 
-{  pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = [ pkgs.kmonad ];
 
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.systemd1.manage-units" &&
+          (action.lookup("unit") == "kmonad-myKMonadOutput.service" ||
+           action.lookup("unit") == "kmonad-myKMonadOutput.path") &&
+          subject.user == "charlotte") {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
   services.kmonad = {
-   enable = true;
-     keyboards = {
-       myKMonadOutput = {
-         device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-         config = builtins.readFile ./colemakHomerowMods.kbd;
-       };
-     };
+    enable = true;
+    keyboards = {
+      myKMonadOutput = {
+        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+        config = builtins.readFile ./colemakHomerowMods.kbd;
+      };
+    };
   };
 }
-
