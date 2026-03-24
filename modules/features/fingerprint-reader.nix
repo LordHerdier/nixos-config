9e0cfb50eaf85672f6a9ecc0deec995a387a9a58
charlotte whittleman <charlotte@whittleman.net>
2026-03-24 13:56:33 -0500

    Add polkit policy to fprintd for bitwarden

--- modules/features/fingerprint-reader.nix
+++ modules/features/fingerprint-reader.nix
@@ -31,4 +31,27 @@
 
   security.pam.services.sudo.fprintAuth = true;
   security.pam.services.login.fprintAuth = true;
+  security.pam.services.polkit-1.fprintAuth = true;
+
+  environment.etc."polkit-1/actions/com.bitwarden.Bitwarden.policy" = {
+    text = ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE policyconfig PUBLIC
+       "-//freedesktop//DTD PolicyKit Policy Configuration 1.0//EN"
+       "http://www.freedesktop.org/standards/PolicyKit/1.0/policyconfig.dtd">
+
+      <policyconfig>
+          <action id="com.bitwarden.Bitwarden.unlock">
+            <description>Unlock Bitwarden</description>
+            <message>Authenticate to unlock Bitwarden</message>
+            <defaults>
+              <allow_any>no</allow_any>
+              <allow_inactive>no</allow_inactive>
+              <allow_active>auth_self</allow_active>
+            </defaults>
+          </action>
+      </policyconfig>
+    '';
+    mode = "0644";
+  };
 }
