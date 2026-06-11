5ed64fcbb1636765872e0d2f29d0ddc497bad69e
charlotte whittleman <charlotte@whittleman.net>
2026-06-11 18:22:49 -0500

    temporarily disable qylock sddm

--- modules/features/qylock.nix
+++ modules/features/qylock.nix
@@ -3,10 +3,18 @@
 { ... }:
 
 {
+
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+
   programs.qylock = {
     enable = true;
-    theme = "pixel-night-city";
-    sddmTheme = "pixel-night-city";
+    theme = "clockwork/orbital";
+    sddm.enable = false;
+    quickshell.enable = true;
+    themeOptions = {
+      osu.gameMode = "menu";
+    };
   };
 
 }
