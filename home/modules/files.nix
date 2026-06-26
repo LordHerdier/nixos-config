52ea137492f71f44353db6b9f75285c69622199b
Charlie Whittleman <brennwh@siue.edu>
2026-06-26 08:20:26 -0500

    Concord keymap home manager

--- home/modules/files.nix
+++ home/modules/files.nix
@@ -18,6 +18,52 @@
       source = "${dotfiles}/yazi/.config/yazi";
       recursive = true;
     };
-   
+
+    ".config/concord/keymap.toml".text = ''
+      # keymap.toml -- Concord, Colemak layout
+      # Only non-default values are specified here.
+      #
+      # Colemak translation:
+      #   e = down  (was j)    u = up    (was k)
+      #   n = left  (was h)    i = right (was l)
+      #   k = compose          (was i; mirrors nvf k -> insert mode)
+      #   E = half-page down   U = half-page up  (mirrors nvf 5j/5k)
+      #   N = scroll-horiz L   I = scroll-horiz R
+
+      [keymap]
+      # -- Navigation --
+      SelectNext = "e"
+      SelectPrevious = "u"
+      CycleFocusNext = { keys = ["tab", "i", "right"] }
+      CycleFocusPrevious = { keys = ["<S-tab>", "n", "left"] }
+      HalfPageDown = { keys = ["<C-d>", "E"] }
+      HalfPageUp = { keys = ["<C-u>", "U"] }
+      ScrollHorizontalLeft = "N"
+      ScrollHorizontalRight = "I"
+      ResizePaneLeft = { keys = ["<A-n>", "<A-left>"] }
+      ResizePaneRight = { keys = ["<A-i>", "<A-right>"] }
+      ToggleGuildPane = { keys = ["<leader>1", "<leader>e"] }
+
+      # -- Actions --
+      StartComposer = "k"
+      EditMessage = "a"
+
+      [keymap.groups]
+      "<leader>v" = "Voice"
+
+      [keymap.guild_actions]
+      MuteServer = { keys = ["x"], description = "mute server" }
+
+      [keymap.channel_actions]
+      MuteChannel = { keys = ["x"], description = "mute channel" }
+
+      [keymap.message_actions]
+      EditMessage = "a"
+      ShowReactionUsers = "z"
+
+      [keymap.composer]
+      OpenEditor = "<C-o>"
+    '';
+
   };
 }
